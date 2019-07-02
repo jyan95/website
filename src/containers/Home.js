d4196dc8ed1dba5bc43ff1aad599ef6e25051c71
jyan95 <jyan0602@gmail.com>
2019-07-02 17:03:08 -0400

    working on project cards, fix links / objects in home.js as well

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,28 +1,21 @@
 import React from 'react';
 import Experience from '../components/Experience';
 import Education from '../components/Education';
+import ProjectCard from '../components/ProjectCard';
 
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Hidden from '@material-ui/core/Hidden';
 import Link from '@material-ui/core/Link';
-import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider';
 import Container from '@material-ui/core/Container';
 
-import Markdown from '../components/Markdown';
-import resume from '../assets/resume.md';
-import education from '../assets/education.md';
+// import Markdown from '../components/Markdown';
+// import resume from '../assets/resume.md';
+// import education from '../assets/education.md';
 
 
 const useStyles = makeStyles(theme => ({
@@ -93,35 +86,58 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const sections = [
-  {name: 'home', url: '/'},
-  {name: 'portfolio', url: '/portfolio'},
-  {name: 'resume', url: 'https://drive.google.com/file/d/1JXaUU60BelPoo6Kz2DIpdsfQk00NQam_/view?usp=sharing'}
+  {
+    name: 'home',
+    url: '/'},
+  {
+    name: 'portfolio',
+    url: '/portfolio'},
+  {
+    name: 'resume',
+    url: 'https://drive.google.com/file/d/1JXaUU60BelPoo6Kz2DIpdsfQk00NQam_/view?usp=sharing'},
+  {
+    name: 'about',
+    url: '/about'
+  }
 ];
 
 
 
 const latestProjects = [
   {
+    url: 'https://imgur.com/NXjFuUZ',
     title: 'DealPal',
-    date: 'June 2019',
-    description:
-      'Mobile optimized app designed to locate and track nearby deals.',
+    width: '30%',
   },
   {
+    url: 'https://imgur.com/p9mCAIP',
     title: 'mise en ++',
-    date: 'May 2019',
-    description:
-      'Single page web app designed to help with your kitchen adventures!',
+    width: '30%',
+  },
+  {
+    url: 'https://imgur.com/VP1vVyL',
+    title: 'space x cookie monster',
+    width: '30%',
   },
 ];
 
 const projects = [
-  { name: 'DealPal', url: 'https://github.com/jyan95/project-deal-locator' },
-  { name: 'mise en ++', url: 'https://github.com/jyan95/project-mise-en-plusplus' },
-  { name: 'space x cookie monster', url: 'https://github.com/jyan95/project-space-x-cookiemonster'}
+  {
+    url: 'https://imgur.com/NXjFuUZ',
+    title: 'DealPal',
+    width: '30%',
+  },
+  {
+    url: 'https://imgur.com/p9mCAIP',
+    title: 'mise en ++',
+    width: '30%',
+  },
+  {
+    url: 'https://imgur.com/VP1vVyL',
+    title: 'space x cookie monster',
+    width: '30%',
+  },
 ];
-
-
 
 const technologies = [
   'JavaScript ES6',
@@ -134,7 +150,6 @@ const technologies = [
   'Heroku',
   'AJAX',
   'Material UI / Semantic / Bootstrap',
-  'Mobile First Thinking',
   'Illustrator'
 ]
 
@@ -160,6 +175,7 @@ const Home = () => {
       <Container maxWidth="lg">
         <Toolbar component="nav" variant="dense" className={classes.toolbar}>
           {sections.map(section => (
+            section.name === 'resume' ?
             <Link
               color="inherit"
               noWrap
@@ -167,6 +183,17 @@ const Home = () => {
               variant="body2"
               href={section.url}
               target='new'
+              className={classes.toolbarLink}
+            >
+              {section.name}
+            </Link>
+            :
+            <Link
+              color="inherit"
+              noWrap
+              key={section.name}
+              variant="body2"
+              href={section.url}
               className={classes.toolbarLink}
             >
               {section.name}
@@ -205,36 +232,10 @@ const Home = () => {
           </Typography>
           <Divider />
           <br/>
-          <Grid container spacing={4} className={classes.cardGrid}>
-            {latestProjects.map(project => (
-              <Grid item key={project.title} xs={12} md={6}>
-                <CardActionArea component="a" href="#">
-                  <Card className={classes.card}>
-                    <div className={classes.cardDetails}>
-                      <CardContent>
-                        <Typography component="h2" variant="h5">
-                          {project.title}
-                        </Typography>
-                        <Typography variant="subtitle1" color="textSecondary">
-                          {project.date}
-                        </Typography>
-                        <Typography variant="subtitle1" paragraph>
-                          {project.description}
-                        </Typography>
-                        <Typography variant="subtitle1" color="primary">
-                          Continue reading...
-                        </Typography>
-                      </CardContent>
-                    </div>
-                    <Hidden xsDown>
-                      <CardMedia
-                        className={classes.cardMedia}
-                        image="https://source.unsplash.com/random"
-                        title="Image title"
-                      />
-                    </Hidden>
-                  </Card>
-                </CardActionArea>
+          <Grid container direction='column'>
+            {latestProjects.map(p => (
+              <Grid item key={p.title} xs={12} md={12}>
+                <ProjectCard project={p}/>
               </Grid>
             ))}
           </Grid>
@@ -298,11 +299,8 @@ const Home = () => {
           <Typography variant="h6" align="center" gutterBottom>
             Footer
           </Typography>
-          <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-            Something here to give the footer a purpose!
-          </Typography>
-          <Typography variant="body2" color="textSecondary" align="center">
-            © 2019 Joshua Yan.&nbsp;All rights reserved. Fullstack Developer
+          <Typography variant="subtitle2" align="center" color="textSecondary" component="p">
+            this website is a work in progress
           </Typography>
         </Container>
       </footer>
