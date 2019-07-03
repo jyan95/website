e94baa9a1a79399d5960b28aff365516d8ea1178
jyan95 <jyan0602@gmail.com>
2019-07-03 00:58:19 -0400

    Updates

---
+++ precache-manifest.5a8c050cf085245a2709c17a6639158c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62f3bce6f09060bcd3aa231b2ba65166",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f81c578956b3be6b14e5",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c9a7e210e9ef3e99e705",
+    "url": "/static/js/2.0aa6d5f0.chunk.js"
+  },
+  {
+    "revision": "f81c578956b3be6b14e5",
+    "url": "/static/js/main.a2f3382f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
