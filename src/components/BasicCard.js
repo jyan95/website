1eddcc4c78d327a64a50fc86a931b162fb5ab41a
jyan95 <jyan0602@gmail.com>
2024-02-17 00:26:33 -0500

    wip

--- src/components/BasicCard.js
+++ src/components/BasicCard.js
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import Grid from '@material-ui/core/Grid';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import Grid from "@material-ui/core/Grid";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
 // import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import GithubIcon from '../assets/github.png';
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import GithubIcon from "../assets/github.png";
 
 const useStyles = makeStyles({
   card: {
     minWidth: 275,
-    minHeight: 275
+    minHeight: 275,
   },
   footer: {
     paddingBottom: 12,
@@ -26,10 +26,10 @@ const BasicCard = (props) => {
   return (
     <Card className={classes.card}>
       <Grid container>
-        <Grid item container direction='row' justify='flex-end'>
+        <Grid item container direction="row" justify="flex-end">
           <CardActions>
-            <IconButton href={props.project.url} target='new'>
-              <img src={GithubIcon} style={{width:25}} alt='Github'/>
+            <IconButton href={props.project.url} target="new">
+              <img src={GithubIcon} style={{ width: 25 }} alt="Github" />
             </IconButton>
           </CardActions>
         </Grid>
@@ -48,14 +48,18 @@ const BasicCard = (props) => {
               <Typography variant="body2" component="p">
                 {props.project.description}
               </Typography>
-              <br/>
+              <br />
             </CardContent>
             <CardContent className={classes.footer}>
-              <Typography variant="body2" component="p" style={{
-                fontFamily: 'Roboto Mono, monospace',
-                fontSize: 11,
-              }}>
-                {props.project.tech.map(t => `${t} `)}
+              <Typography
+                variant="body2"
+                component="p"
+                style={{
+                  fontFamily: "Helvetica Neue",
+                  fontSize: 11,
+                }}
+              >
+                {props.project.tech.map((t) => `${t} `)}
               </Typography>
             </CardContent>
           </Grid>
@@ -63,6 +67,6 @@ const BasicCard = (props) => {
       </Grid>
     </Card>
   );
-}
+};
 
 export default BasicCard;
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,90 +1,36 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
 import React from "react";
 
-import Navbar from "../components/Navbar";
-import Experience from "../components/Experience";
-import Education from "../components/Education";
-
 import { makeStyles } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import Divider from "@material-ui/core/Divider";
 
-import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
-import Divider from "@material-ui/core/Divider";
-import Container from "@material-ui/core/Container";
 
 const useStyles = makeStyles((theme) => ({
-  banner: {
-    position: "relative",
-    backgroundColor: "#24283b",
-    color: theme.palette.common.white,
-    marginTop: theme.spacing(6),
-    paddingBottom: theme.spacing(4),
-    backgroundImage: "url(https://i.imgur.com/WBlNmxU.jpg)",
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "center",
+  gridItem: {
+    marginTop: "48px",
   },
-  overlay: {
-    position: "absolute",
-    top: 0,
-    bottom: 0,
-    right: 0,
-    left: 0,
-    backgroundColor: "rgba(0,0,0,.4)",
-  },
-  bannerContent: {
-    position: "relative",
-    padding: theme.spacing(3),
-    [theme.breakpoints.up("md")]: {
-      padding: theme.spacing(6),
-      paddingRight: 0,
-      paddingLeft: 0,
-    },
-  },
-  mainGrid: {
-    marginTop: 0,
-    color: "#b8b4d8",
-    backgroundColor: "#24283b",
-  },
-  technologiesBox: {
-    color: "#97c566",
-    padding: theme.spacing(2),
-    backgroundColor: "#282e42",
-    borderRadius: "6px",
-  },
-  sidebarSection: {
+  summary: { lineHeight: "1.4", fontSize: "4.5vw", fontWeight: 600 },
+  italics: {
     marginTop: theme.spacing(3),
-    backgroundColor: "#24283b",
   },
-  divider: {
-    backgroundColor: "#97c566",
+  focus: {
+    marginTop: "48px",
+    lineHeight: "2.5",
+    fontWeight: 600,
+    fontSize: "3.3vw",
   },
+  projects: {
+    lineHeight: 1.5,
+    fontWeight: 500,
+    fontSize: "28px",
+    marginTop: "24px",
+  },
+  philo: {},
+  divider: {},
 }));
-
-const about = (
-  <p>
-    I was born in Michigan, and moved to China when I was 10.
-    <br />
-    I attended international schools in Wuxi, Suzhou, and Shanghai.
-    <br />
-    I moved back to Michigan for undergrad, and to New York City shortly after.
-    <br />
-    I walked dogs and worked a dead-end job for a bit, then took myself to a
-    code camp.
-    <br />
-    I currently work at Kinetik as a Senior Engineer / Project Lead.
-    <br />I spend most of my free time climbing, swimming, and learning about
-    web3.
-  </p>
-);
-
-const technologies = [
-  "JS (Node, Express, React, Redux, Jest)",
-  "AWS (SLS, SQS, SNS)",
-  "MongoDB (Mongoose)",
-];
 
 const Home = () => {
   const classes = useStyles();
@@ -92,115 +38,75 @@ const Home = () => {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Navbar />
-      {/* Start Banner */}
-
-      <Paper className={classes.banner}>
-        {
-          <img
-            style={{ display: "none" }}
-            src="https://i.imgur.com/WBlNmxU.jpg"
-            alt="background"
-          />
-        }
-        <div className={classes.overlay} />
-        <Container maxWidth="md">
-          <div className={classes.bannerContent}>
-            <Typography
-              component="h1"
-              variant="h3"
-              color="inherit"
-              style={{ fontFamily: "Roboto Mono, monospace" }}
-              gutterBottom
-            >
-              <b>joshua yan</b>
-            </Typography>
-            <Typography
-              variant="h5"
-              color="inherit"
-              paragraph
-              style={{ fontFamily: "Roboto Mono, monospace" }}
-            >
-              nyc based software engineer
-            </Typography>
-          </div>
-        </Container>
-      </Paper>
-      {/* End banner */}
-      <Container
-        maxWidth="md"
-        style={{ color: "#b8b4d8", backgroundColor: "#24283b" }}
-      >
-        <main>
-          <div id="about">
-            <Typography
-              variant="h6"
-              style={{
-                paddingTop: "20px",
-                fontFamily: "Roboto Mono, monospace",
-              }}
-              gutterBottom
-            >
-              <b style={{ color: "#73d9c8" }}>// about</b>
-            </Typography>
-            <Divider className={classes.divider} />
-            <div style={{ padding: "16px 16px 0px 16px" }}>
-              <Typography variant="subtitle1">{about}</Typography>
+      <Grid container>
+        <Grid container>
+          <Grid item xs={12} md={10}>
+            <div style={{ padding: "56px 16px 16px 56px" }} id="title">
+              <Typography style={{ fontSize: "2rem" }}>Joshua Yan</Typography>
             </div>
-          </div>
-          <Grid container spacing={5} className={classes.mainGrid}>
-            {/* Experience and Education */}
-            <Grid item xs={12} md={8}>
-              <div id="experience">
-                <Typography
-                  variant="h6"
-                  gutterBottom
-                  style={{ fontFamily: "Roboto Mono, monospace" }}
-                >
-                  <b style={{ color: "#73d9c8" }}>// experience</b>
-                </Typography>
-                <Divider className={classes.divider} />
-                <Experience />
-              </div>
-            </Grid>
-            {/* Side Content */}
-            <Grid item xs={12} md={4}>
-              <Paper elevation={0} className={classes.technologiesBox}>
-                <Typography
-                  variant="h6"
-                  style={{ fontFamily: "Roboto Mono, monospace" }}
-                  gutterBottom
-                >
-                  techstack
-                </Typography>
-                <Typography variant="body2">
-                  {technologies.map((t) => {
-                    return (
-                      <span key={t} style={{ color: "#b8b4d8" }}>
-                        - {t}
-                        <br />
-                      </span>
-                    );
-                  })}
-                </Typography>
+          </Grid>
+        </Grid>
+        <Grid container>
+          <Grid item xs={12} md={10}>
+            <div style={{ padding: "64px 16px 0px 56px" }} id="summary">
+              <Typography className={classes.summary}>
+                Builder, swimmer, pianist, climber.
                 <br />
-              </Paper>
-            </Grid>
-            {/* End Side Content */}
-            <Grid item xs={12} md={12}>
+                Currently leading a team at Kinetik, designing and delivering
+                software solutions for managed care transportation.
+              </Typography>
+            </div>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} className={classes.gridItem}>
+          {/* Current areas of focus */}
+          <Grid item xs={4} md={4}></Grid>
+          <Grid item xs={8} md={8}>
+            <Typography variant="h3" className={classes.focus} gutterBottom>
+              Project management
+              <br />
+              System Design
+              <br />
+              Code Review
+              <br />
+              UI/UX, QA
+            </Typography>
+          </Grid>
+          <Grid container spacing={2} className={classes.gridItem}>
+            {/* Current areas of focus */}
+            {/* Projects */}
+            <Grid item xs={4} md={4}></Grid>
+            <Grid item xs={8} md={7}>
+              <Typography className={classes.projects} gutterBottom>
+                A trip scheduling platform for health systems.
+              </Typography>
+              <Typography className={classes.projects} gutterBottom>
+                A trip dispatch platform for transportation networks.
+              </Typography>
               <Typography
-                variant="h6"
-                style={{ fontFamily: "Roboto Mono, monospace" }}
+                className={classes.projects}
+                style={{ lineHeight: 1.5 }}
                 gutterBottom
               >
-                <b style={{ color: "#73d9c8" }}>// education</b>
+                A claims generation, attestation, and submission platform for
+                transportation providers.
               </Typography>
-              <Divider className={classes.divider} />
-              <Education />
             </Grid>
           </Grid>
-        </main>
-      </Container>
+          <Grid container spacing={2} className={classes.gridItem}>
+            <Grid item xs={2} md={4}></Grid>
+            <Grid item xs={10} md={6}>
+              <Typography className={classes.philo} gutterBottom>
+                Embracing individuality and autonomy, personal growth lies at
+                the forefront of my leadership philosophy. I empower team
+                members to identify and leverage their unique strengths,
+                cultivating ownership and initiative toward shared goals.
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </React.Fragment>
   );
 };
