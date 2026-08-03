51b135a16d73c6bca6c69ad7b8ae7f9df4edb406
jyan95 <joshua@kinetik.care>
2026-08-03 14:04:39 -0400

    style: gmail M-envelope glyph for the email link, static gain 0.32 -> 0.16

--- src/components/links/LinkList.jsx
+++ src/components/links/LinkList.jsx
@@ -48,7 +48,8 @@ function ResumeGlyph() {
   );
 }
 
-function MailGlyph() {
+/* The Gmail M-envelope silhouette (it's a gmail address), stroked as line art. */
+function GmailGlyph() {
   return (
     <svg
       className="glyph"
@@ -56,15 +57,10 @@ function MailGlyph() {
       aria-hidden="true"
       focusable="false"
     >
-      <rect
-        x="2"
-        y="4"
-        width="20"
-        height="16"
-        rx="2"
+      <path
+        d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z"
         vectorEffect="non-scaling-stroke"
       />
-      <path d="m22 6-10 7L2 6" vectorEffect="non-scaling-stroke" />
     </svg>
   );
 }
@@ -103,7 +99,7 @@ const LINKS = [
     href: 'mailto:jyan0602@gmail.com',
     ariaLabel: 'Email Joshua',
     external: false,
-    Glyph: MailGlyph,
+    Glyph: GmailGlyph,
   },
   {
     href: 'https://hyperbolic.nyc/',
